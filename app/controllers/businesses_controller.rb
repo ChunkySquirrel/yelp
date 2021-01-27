dd22d2bd563657f3df54b0e428d152d7e46e0b6b
ChunkySquirrel <matejmobile516@gmail.com>
2021-01-27 01:40:03 +0100

    root path on delete

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -64,7 +64,7 @@ end
   def destroy
     @business.destroy
     respond_to do |format|
-      format.html { redirect_to businesses_url, notice: "Business was successfully destroyed." }
+      format.html { redirect_to root_path, notice: "Business was successfully destroyed." }
       format.json { head :no_content }
     end
   end
