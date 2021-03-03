de751280f5a180adaedf4a96d1a83d1556d8e92d
ChunkySquirrel <matejmobile516@gmail.com>
2021-03-03 08:19:20 +0100

    Review link

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,13 +4,14 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true
   validates :comment, presence:true, length: {maximum: 500}
+
   validates :user_id, :uniqueness => { :scope => :business_id,
     :message => " may only write one review per business. You can only edit or delete your existing review" }
 
   has_attached_file :image, styles: { medium: "500x400#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  
+
   def blank_stars
     5 - rating.to_i
   end
