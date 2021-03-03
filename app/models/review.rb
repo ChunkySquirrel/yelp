94103d9c9ada4a486d1c98a3e566a8fece713ea6
ChunkySquirrel <matejmobile516@gmail.com>
2021-03-03 08:44:46 +0100

    Remove img

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,9 +8,6 @@ class Review < ApplicationRecord
   validates :user_id, :uniqueness => { :scope => :business_id,
     :message => " may only write one review per business. You can only edit or delete your existing review" }
 
-  has_attached_file :image, styles: { medium: "500x400#"}
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-
 
   def blank_stars
     5 - rating.to_i
